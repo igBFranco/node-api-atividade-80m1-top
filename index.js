9e7c87738d1605a0773b2da11ece5f9c3e44e3e1
Jailson <jailson.ferens@compasso.com.br>
2022-09-27 16:24:18 -0300

    Altrerando index

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const config = require("./config");
 const plateRoutes = require('./routes/plateRoutes');
 const restaurantRoutes = require('./routes/restaurantRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const deliveryRoutes = require('./routes/deliveryRoutes');
 
 const app = express();
 
@@ -13,6 +16,9 @@ app.use(cors());
 
 app.use('/api/plates', plateRoutes);
 app.use('/api/restaurants', restaurantRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/delivery', deliveryRoutes);
 
 app.listen(config.port, () => {
     console.log(`API rodando em ${config.url}`);
